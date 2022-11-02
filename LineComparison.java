5b4ebd1f46a1c26e99f5efe69ecd4ab7573a3f82
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-02 14:58:20 +0530

    Added: Find the length of line from its end point

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,29 @@
 package com.bridgelab.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
-	public static void main(String[] args) {
-		System.out.println("Well come to Line Comparison Program");
+	int x1, x2, y1, y2;
+	double distance;
+
+	public double calculateLength() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter x1 point");
+		x1 = sc.nextInt();
+		System.out.println("enter y1 point");
+		y1 = sc.nextInt();
+		System.out.println("enter x2point");
+		x2 = sc.nextInt();
+		System.out.println("enter y2 point");
+		y2 = sc.nextInt();
+		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		return distance;
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Well come to Line Comparison Program");
+		LineComparison line = new LineComparison();
+		double Length = line.calculateLength();
+		System.out.println("Lenght of line = " + Length);
+	}
 }
