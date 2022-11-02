7f2a97530bb184a39e6c145cfc001341405820ee
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-02 14:15:18 +0530

    Added: Well come message for Line Comparison

---
+++ LineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelab.linecomparison;
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Well come to Line Comparison Program");
+	}
+
+}
