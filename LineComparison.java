400f7420935f38b5928bd7d79437518b102bbefc
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-02 15:24:45 +0530

    Added: Checking two line length are equal or not equal

--- LineComparison.java
+++ LineComparison.java
@@ -19,11 +19,29 @@ public class LineComparison {
 		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		return distance;
 	}
+	public static void equalityCheck(double L1,double L2) {
+		String str1=String.valueOf(L1);
+			String str2=String.valueOf(L2);
+    if (str1.equals(str2)) {
+    	System.out.println("Two Lines are of equal lenght");
+    }
+    else {
+    	System.out.println("Two Lines are of not equal lenght");
+    }
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Well come to Line Comparison Program");
-		LineComparison line = new LineComparison();
-		double Length = line.calculateLength();
-		System.out.println("Lenght of line = " + Length);
+		LineComparison line1 = new LineComparison();
+		System.out.println("Enter details of first line");
+		double Length1 = line1.calculateLength();
+		System.out.println("Lenght of line1 = " + Length1);
+		LineComparison line2 = new LineComparison();
+		System.out.println("Enter details of second line");
+		double Length2 = line2.calculateLength();
+		System.out.println("Lenght of line2 = " + Length2);
+		equalityCheck(Length1,Length2);
+		
 	}
 }
